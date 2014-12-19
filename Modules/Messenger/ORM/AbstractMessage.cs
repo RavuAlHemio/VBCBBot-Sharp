3122f679b8f665ccfcd5366377007794d3acb54c
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2014-12-19 20:36:21 +0100

    Revert "Messenger: don't do inheritance with Message types; it confuses EF"
    This reverts commit b029e33de4ad522905dd23b8818f66e55d989f57.

---
+++ Modules/Messenger/ORM/AbstractMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Messenger.ORM
+{
+    public abstract class AbstractMessage
+    {
+        [Key]
+        [Required]
+        [Column("message_id")]
+        [MaxLength(255)]
+        public long ID { get; set; }
+
+        [Required]
+        [Column("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [Required]
+        [Column("sender_original")]
+        [MaxLength(255)]
+        public string SenderOriginal { get; set; }
+
+        [Required]
+        [Column("recipient_folded")]
+        [MaxLength(255)]
+        public string RecipientFolded { get; set; }
+
+        [Required]
+        [Column("body")]
+        [MaxLength]
+        public string Body { get; set; }
+
+        public AbstractMessage()
+        {
+        }
+
+        public AbstractMessage(AbstractMessage other)
+        {
+            this.ID = other.ID;
+            this.Timestamp = other.Timestamp;
+            this.SenderOriginal = other.SenderOriginal;
+            this.RecipientFolded = other.RecipientFolded;
+            this.Body = other.Body;
+        }
+    }
+}
--- Modules/Messenger/ORM/Message.cs
+++ Modules/Messenger/ORM/Message.cs
@@ -1,44 +1,19 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Messenger.ORM
 {
     [Table("messages", Schema = "messenger")]
-    public class Message : IMessage
+    public class Message : AbstractMessage
     {
-        [Key]
-        [Required]
-        [Column("message_id")]
-        [MaxLength(255)]
-        public long ID { get; set; }
-
-        [Required]
-        [Column("timestamp")]
-        public DateTime Timestamp { get; set; }
-
-        [Required]
-        [Column("sender_original")]
-        [MaxLength(255)]
-        public string SenderOriginal { get; set; }
-
-        [Required]
-        [Column("recipient_folded")]
-        [MaxLength(255)]
-        public string RecipientFolded { get; set; }
-
-        [Required]
-        [Column("body")]
-        [MaxLength]
-        public string Body { get; set; }
-
         public Message()
+            : base()
         {
         }
 
-        public Message(IMessage other)
+        public Message(AbstractMessage other)
+            : base(other)
         {
-            MessageUtils.CopyMessage(other, this);
         }
     }
 }
