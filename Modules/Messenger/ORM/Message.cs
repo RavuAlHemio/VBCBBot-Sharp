b029e33de4ad522905dd23b8818f66e55d989f57
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2014-12-19 20:30:00 +0100

    Messenger: don't do inheritance with Message types; it confuses EF

--- Modules/Messenger/ORM/Message.cs
+++ Modules/Messenger/ORM/Message.cs
@@ -1,19 +1,44 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Messenger.ORM
 {
     [Table("messages", Schema = "messenger")]
-    public class Message : AbstractMessage
+    public class Message : IMessage
     {
+        [Key]
+        [Required]
+        [Column("message_id")]
+        [MaxLength(255)]
+        public long ID { get; set; }
+
+        [Required]
+        [Column("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [Required]
+        [Column("sender_original")]
+        [MaxLength(255)]
+        public string SenderOriginal { get; set; }
+
+        [Required]
+        [Column("recipient_folded")]
+        [MaxLength(255)]
+        public string RecipientFolded { get; set; }
+
+        [Required]
+        [Column("body")]
+        [MaxLength]
+        public string Body { get; set; }
+
         public Message()
-            : base()
         {
         }
 
-        public Message(AbstractMessage other)
-            : base(other)
+        public Message(IMessage other)
         {
+            MessageUtils.CopyMessage(other, this);
         }
     }
 }
