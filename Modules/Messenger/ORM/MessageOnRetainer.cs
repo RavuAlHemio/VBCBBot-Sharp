7b2a4552a6861d9f7cbf4f384539f834087902d5
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2014-12-19 20:42:55 +0100

    re-apply Messenger: don't do inheritance with Message types; it confuses EF

--- Modules/Messenger/ORM/MessageOnRetainer.cs
+++ Modules/Messenger/ORM/MessageOnRetainer.cs
@@ -1,19 +1,44 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Messenger.ORM
 {
     [Table("messages_on_retainer", Schema = "messenger")]
-    public class MessageOnRetainer : AbstractMessage
+    public class MessageOnRetainer : IMessage
     {
+        [Key]
+        [Required]
+        [Column("message_id")]
+        [MaxLength(255)]
+        public long ID { get; set; }
+
+        [Required]
+        [Column("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [Required]
+        [Column("sender_original")]
+        [MaxLength(255)]
+        public string SenderOriginal { get; set; }
+
+        [Required]
+        [Column("recipient_folded")]
+        [MaxLength(255)]
+        public string RecipientFolded { get; set; }
+
+        [Required]
+        [Column("body")]
+        [MaxLength]
+        public string Body { get; set; }
+
         public MessageOnRetainer()
-            : base()
         {
         }
 
-        public MessageOnRetainer(AbstractMessage other)
-            : base(other)
+        public MessageOnRetainer(IMessage other)
         {
+            MessageUtils.CopyMessage(other, this);
         }
     }
 }
---
+++ Modules/Messenger/ORM/MessageUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace Messenger.ORM
+{
+    public static class MessageUtils
+    {
+        public static void CopyMessage(IMessage fromMessage, IMessage toMessage)
+        {
+            toMessage.ID = fromMessage.ID;
+            toMessage.Timestamp = fromMessage.Timestamp;
+            toMessage.SenderOriginal = fromMessage.SenderOriginal;
+            toMessage.RecipientFolded = fromMessage.RecipientFolded;
+            toMessage.Body = fromMessage.Body;
+        }
+    }
+}
