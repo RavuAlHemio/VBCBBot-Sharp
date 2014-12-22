e2363075935cc01de5f29e5358fbdb6e662ba926
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2014-12-22 12:10:24 +0100

    implement Allograph (untested)

---
+++ Modules/Allograph/Allograph.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Remoting.Messaging;
+using Newtonsoft.Json.Linq;
+using VBCBBot;
+
+namespace Allograph
+{
+    public class Allograph : ModuleV1
+    {
+        private AllographConfig _config;
+
+        public Allograph(ChatboxConnector connector, JObject config)
+            : base(connector)
+        {
+            _config = new AllographConfig(config);
+        }
+
+        protected override void ProcessUpdatedMessage(ChatboxMessage message, bool isPartOfInitialSalvo = false, bool isEdited = false,
+            bool isBanned = false)
+        {
+            if (isPartOfInitialSalvo || isEdited || isBanned)
+            {
+                return;
+            }
+
+            var originalBody = message.BodyBBCode;
+            var newBody = originalBody;
+
+            foreach (var repl in _config.Replacements)
+            {
+                newBody = repl.Regex.Replace(newBody, repl.ReplacementString);
+            }
+
+            if (newBody != originalBody)
+            {
+                Connector.SendMessage(newBody);
+            }
+        }
+    }
+}
