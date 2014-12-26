a6a353ccc8a17dbc3bd1feb3fa2b0a3204721ec5
Ondřej Hošek <ondra.hosek@gmail.com>
2014-12-26 19:46:19 +0100

    more tests

---
+++ BotTests/ArraySplitTests.cs
@@ -0,0 +1,167 @@
+﻿using System.Linq;
+using VBCBBot;
+using Xunit;
+
+namespace BotTests
+{
+    public class ArraySplitTests
+    {
+        [Fact]
+        public void SplitEmptyArray()
+        {
+            var arr = new byte[0];
+            var ret = arr.Split(new [] { (byte)0 }).ToList();
+
+            Assert.Equal(1, ret.Count);
+            Assert.Equal(0, ret[0].Length);
+        }
+
+        [Fact]
+        public void SplitIntoTwo()
+        {
+            var arr = new byte[] { 1, 2, 3 };
+            var ret = arr.Split(new [] { (byte)2 }).ToList();
+
+            Assert.Equal(2, ret.Count);
+            Assert.Equal(1, ret[0].Length);
+            Assert.Equal(1, ret[0][0]);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(3, ret[1][0]);
+        }
+
+        [Fact]
+        public void SingleElementArrays()
+        {
+            var arr = new byte[] { 1, 2, 3, 2, 4, 2, 5 };
+            var ret = arr.Split(new [] { (byte)2 }).ToList();
+
+            Assert.Equal(4, ret.Count);
+            Assert.Equal(1, ret[0].Length);
+            Assert.Equal(1, ret[0][0]);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(3, ret[1][0]);
+            Assert.Equal(1, ret[2].Length);
+            Assert.Equal(4, ret[2][0]);
+            Assert.Equal(1, ret[3].Length);
+            Assert.Equal(5, ret[3][0]);
+        }
+
+        [Fact]
+        public void BeginsWithSplitter()
+        {
+            var arr = new byte[] { 2, 1, 2, 3 };
+            var ret = arr.Split(new [] { (byte)2 }).ToList();
+
+            Assert.Equal(3, ret.Count);
+            Assert.Equal(0, ret[0].Length);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(1, ret[1][0]);
+            Assert.Equal(1, ret[2].Length);
+            Assert.Equal(3, ret[2][0]);
+        }
+
+        [Fact]
+        public void EndsWithSplitter()
+        {
+            var arr = new byte[] { 1, 2, 3, 2 };
+            var ret = arr.Split(new [] { (byte)2 }).ToList();
+
+            Assert.Equal(3, ret.Count);
+            Assert.Equal(1, ret[0].Length);
+            Assert.Equal(1, ret[0][0]);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(3, ret[1][0]);
+            Assert.Equal(0, ret[2].Length);
+        }
+
+        [Fact]
+        public void BeginsAndEndsWithSplitter()
+        {
+            var arr = new byte[] { 2, 1, 2, 3, 2 };
+            var ret = arr.Split(new [] { (byte)2 }).ToList();
+
+            Assert.Equal(4, ret.Count);
+            Assert.Equal(0, ret[0].Length);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(1, ret[1][0]);
+            Assert.Equal(1, ret[2].Length);
+            Assert.Equal(3, ret[2][0]);
+            Assert.Equal(0, ret[3].Length);
+        }
+
+        [Fact]
+        public void ExactMaxCount()
+        {
+            var arr = new byte[] { 1, 2, 3, 2, 4 };
+            var ret = arr.Split(new [] { (byte)2 }, 3).ToList();
+
+            Assert.Equal(3, ret.Count);
+            Assert.Equal(1, ret[0].Length);
+            Assert.Equal(1, ret[0][0]);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(3, ret[1][0]);
+            Assert.Equal(1, ret[2].Length);
+            Assert.Equal(4, ret[2][0]);
+        }
+
+        [Fact]
+        public void LowerMaxCount()
+        {
+            var arr = new byte[] { 1, 2, 3, 2, 4 };
+            var ret = arr.Split(new [] { (byte)2 }, 2).ToList();
+
+            Assert.Equal(2, ret.Count);
+            Assert.Equal(1, ret[0].Length);
+            Assert.Equal(1, ret[0][0]);
+            Assert.Equal(3, ret[1].Length);
+            Assert.Equal(3, ret[1][0]);
+            Assert.Equal(2, ret[1][1]);
+            Assert.Equal(4, ret[1][2]);
+        }
+
+        [Fact]
+        public void GreaterMaxCount()
+        {
+            var arr = new byte[] { 1, 2, 3, 2, 4 };
+            var ret = arr.Split(new [] { (byte)2 }, 4).ToList();
+
+            Assert.Equal(3, ret.Count);
+            Assert.Equal(1, ret[0].Length);
+            Assert.Equal(1, ret[0][0]);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(3, ret[1][0]);
+            Assert.Equal(1, ret[2].Length);
+            Assert.Equal(4, ret[2][0]);
+        }
+
+        [Fact]
+        public void ZeroMaxCount()
+        {
+            var arr = new byte[] { 1, 2, 3, 2, 4 };
+            var ret = arr.Split(new [] { (byte)2 }, 0).ToList();
+
+            Assert.Equal(0, ret.Count);
+        }
+
+        [Fact]
+        public void EmptySplitter()
+        {
+            var arr = new byte[] { 1, 2, 3, 2, 4 };
+            var ret = arr.Split(new byte[0]).ToList();
+
+            Assert.Equal(5, ret.Count);
+
+            Assert.Equal(1, ret[0].Length);
+            Assert.Equal(1, ret[1].Length);
+            Assert.Equal(1, ret[2].Length);
+            Assert.Equal(1, ret[3].Length);
+            Assert.Equal(1, ret[4].Length);
+
+            Assert.Equal(1, ret[0][0]);
+            Assert.Equal(2, ret[1][0]);
+            Assert.Equal(3, ret[2][0]);
+            Assert.Equal(2, ret[3][0]);
+            Assert.Equal(4, ret[4][0]);
+        }
+    }
+}
---
+++ BotTests/UrlDecodingTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using VBCBBot;
+using Xunit;
+
+namespace BotTests
+{
+    public class UrlDecodingTests
+    {
+        private void Test(string unplussedResult, string plussedResult, string input)
+        {
+            var inputBytes = Util.Utf8NoBom.GetBytes(input);
+            Assert.Equal(unplussedResult, Util.UrlDecodeToString(inputBytes, Util.Utf8NoBom, plusIsSpace: false));
+            Assert.Equal(plussedResult, Util.UrlDecodeToString(inputBytes, Util.Utf8NoBom, plusIsSpace: true));
+        }
+
+        [Fact]
+        public void EmptyString()
+        {
+            Test("", "", "");
+        }
+
+        [Fact]
+        public void ShortString()
+        {
+            Test("abc", "abc", "abc");
+        }
+
+        [Fact]
+        public void StringWithPluses()
+        {
+            Test("one+two+three", "one two three", "one+two+three");
+        }
+    }
+}
